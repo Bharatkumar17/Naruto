dc5653421e066916001af0e42abe8c23902cd6d0
Bharatkumar17 <70887566+Bharatkumar17@users.noreply.github.com>
2025-06-15 08:54:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,30 +2,30 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = "8012231272:AAFITViiSvq-2hXqbCaS4VBwHahNsim-woE"
+    BOT_TOKEN = "7830984248:AAFgFOWSSFW9GAP0i38LpkLVNb6pb9DWwcE"
     
-    API_ID = 12475131
+    API_ID = 26729193
     
-    API_HASH = "719171e38be5a1f500613837b79c536f"
-    ADMIN_ID = [12475131]
+    API_HASH = "a94598ef642481e35466292df95f251e"
+    ADMIN_ID = [1012164907]
     
-    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://Drmbaby:drmbaby@cluster0.xwlbw7t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     
     DB_NAME = "MY_BOT_DB"
     
-    TXT_LOG = -1002542634912
+    TXT_LOG = -1002607015025
     
-    AUTH_LOG = -1002542634912
+    AUTH_LOG = -1002607015025
     
-    HIT_LOG = -1002542634912
+    HIT_LOG = -1002607015025
     
-    DRM_DUMP = -1002542634912
+    DRM_DUMP = -1002607015025
     # Main channel ki ID
-    CHANNEL = -1002542634912
+    CHANNEL = -1002607015025
     # Channel ka link
-    CH_URL = "https://t.me/gjskisb"
+    CH_URL = "https://t.me/rajsthangkguru1"
     
-    OWNER = "https://t.me/gjskisb"
+    OWNER = "https://t.me/rajsthangkguru1"
     
     THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
    
